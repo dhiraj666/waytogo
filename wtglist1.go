64cbc4da9b674c458c315607f7507bf9a824acce
Dhiraj <dhirajdas.666@gmail.com>
2015-10-03 12:40:21 +0530

    InitialCommit WaytoGo

---
+++ wtglist1.go
@@ -0,0 +1,12 @@
+/*
+	code less, compile quicker , execute faster
+	welcome to the world  of developing in golang
+	NaCl - Native Client in the chrome browser
+	Runs on INtel as well as ARM processors , so it runs under the Android OS
+*/
+
+package main
+
+func main() {
+	println("Hello", "world !!")
+}
