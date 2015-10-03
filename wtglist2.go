55fdc3838f51efff6b62b7f5deb15f7a7a296686
Dhiraj <dhirajdas.666@gmail.com>
2015-10-03 13:18:20 +0530

    waytogoprograms

---
+++ wtglist2.go
@@ -0,0 +1,18 @@
+/*
+There are 25 keywords
+break	default		func        map      struct
+case	defer		goto		package  switch
+chan	else		if          range    type
+const	fallthrough interface   select
+continue for        import      return    var
+
+*/
+// Basic structure and components of a go program
+// A standalone executable belongs to package main
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello, world ")
+}
---
+++ wtglist3.go
@@ -0,0 +1,21 @@
+// factoring applicable to import
+// const, var and type
+
+package main
+
+import (
+	"fmt"
+)
+
+var (
+	a string
+	b int
+)
+
+const (
+	avar = "dhiraj"
+)
+
+func main() {
+	fmt.Println(a, b, avar)
+}
